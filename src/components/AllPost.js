9dce06f03179bd46c301e7184d7669ff19e95ac0
Omar Rmz <omar.cruzr97@gmail.com>
2024-04-02 01:09:17 -0600

    updating allpost

--- src/components/AllPost.js
+++ src/components/AllPost.js
@@ -5,7 +5,6 @@ import '../css/AllPost.css';
 
 const AllPost = () => {
     const [allPostData, setAllPostData] = useState(null);
-    const [selectedImage, setSelectedImage] = useState(null);
 
     useEffect(() =>{
         sanityClient.fetch(
@@ -24,26 +23,15 @@ const AllPost = () => {
         .catch(err => console.error(err));
     }, [])
 
-    const handleMouseEnter = (index) => {
-        setSelectedImage(index);
-    }
-
-    const handleMouseLeave = () => {
-        setSelectedImage(null);
-    }
-
     return (
         <div>
             <div>
                 <h1 style={{display:'flex', justifyContent:'center'}}>Aseorías UTMitas</h1>
                 <h2 style={{display:'flex', justifyContent:'center'}}>Tutores UTMitas</h2>
                 <div className="grid-container">
-                    {allPostData && allPostData.map((post, index) => (
-                        <Link to={'/' + post.slug.current} key={post.slug.current}
-                            className={`grid-item ${selectedImage !== null && selectedImage !== index ? 'opacity-40' : ''}`}
-                            onMouseEnter={() => handleMouseEnter(index)}
-                            onMouseLeave={handleMouseLeave}>
-                            <img src={post.mainImage.asset.url} alt='UTMita' width="160" height="auto" />
+                    {allPostData && allPostData.map((post) => (
+                        <Link to={'/' + post.slug.current} key={post.slug.current} className="grid-item">
+                            <img src={post.mainImage.asset.url} alt='UTMita' width="160" height="auto" className="post-image" />
                             <h2>{post.title}</h2>
                         </Link>
                     ))}
