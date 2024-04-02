a50d98b3da871ae41a334d3e37841d6bec15738f
Omar Rmz <omar.cruzr97@gmail.com>
2024-04-02 00:30:21 -0600

    adding cargando

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
 import './App.css';
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { AllPosts, OnePost } from './components';
+import Cargando from './components/Cargando';
 
 const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  setTimeout(() => {
+    setIsLoading(false);
+  }, 1000);
   return (
     <BrowserRouter>
-      <div>
-        <Routes>
-          <Route Component={AllPosts} path='/' exact/>
-          <Route Component={OnePost} path='/:slug' exact/>
-        </Routes>
-      </div>
+    {isLoading ? (
+        <Cargando url="https://raw.githubusercontent.com/Damanger/Portfolio/main/public/Ardilla.webp" />
+      ) : (
+        <div>
+          <Routes>
+            <Route Component={AllPosts} path='/' exact/>
+            <Route Component={OnePost} path='/:slug' exact/>
+          </Routes>
+        </div>
+      )}
     </BrowserRouter>
   );
 }
---
+++ src/components/Cargando.js
@@ -0,0 +1,23 @@
+import React, { useEffect } from 'react';
+import '../css/cargando.css';
+
+const Cargando = ({ url }) => {
+    useEffect(() => {
+        const timeout = setTimeout(() => {
+            const loader = document.querySelector('.cargando');
+            if (loader) {
+                loader.classList.add('oculto');
+            }
+        }, 1000);
+
+        return () => clearTimeout(timeout);
+    }, []);
+
+    return (
+        <div className="cargando">
+            <img src={url} alt="Cargando..." />
+        </div>
+    );
+};
+
+export default Cargando;
