b844939ee68c604d1d9736ac2c7e0f985acd3eba
Omar Rmz <omar.cruzr97@gmail.com>
2024-04-02 01:27:40 -0600

    updating allpost

--- src/components/AllPost.js
+++ src/components/AllPost.js
@@ -32,7 +32,9 @@ const AllPost = () => {
                     {allPostData && allPostData.map((post) => (
                         <Link to={'/' + post.slug.current} key={post.slug.current} className="grid-item">
                             <img src={post.mainImage.asset.url} alt='UTMita' width="160" height="auto" className="post-image" />
-                            <h2>{post.title}</h2>
+                            <div style={{display:'flex', justifyContent:'center', alignItems:'flex-end', height:'100%'}}>
+                                <h2>{post.title}</h2>
+                            </div>
                         </Link>
                     ))}
                 </div>
