33099b7992470f0c9a739a38511b196681524b23
Omar Rmz <omar.cruzr97@gmail.com>
2024-04-04 10:32:39 -0600

    updating contacto

--- src/components/Contacto.js
+++ src/components/Contacto.js
@@ -4,6 +4,7 @@ import '../css/Contacto.css';
 export default function Contacto() {
     const [fileName, setFileName] = useState("");
     const [imagePreview, setImagePreview] = useState(null);
+    const [carrera, setCarrera] = useState("");
 
     function updateFileName(event) {
         const file = event.target.files[0];
@@ -28,10 +29,22 @@ export default function Contacto() {
             <form className="form">
                 <div className="title">Contáctanos</div>
                 <input type="text" placeholder="Nombre completo" />
-                <input type="text" placeholder="Carrera" />
+                <select value={carrera} onChange={(e) => setCarrera(e.target.value)}>
+                    <option value="" disabled defaultValue>Seleccione su carrera</option>
+                    <option value="Ing. Computación">Ing. Computación</option>
+                    <option value="Ing. Alimentos">Ing. Alimentos</option>
+                    <option value="Ing. Electrónica">Ing. Electrónica</option>
+                    <option value="Ing. Mecatrónica">Ing. Mecatrónica</option>
+                    <option value="Ing. Industrial">Ing. Industrial</option>
+                    <option value="Ing. Física Aplicada">Ing. Física Aplicada</option>
+                    <option value="Ing. Mecánica Automotiz">Ing. Mecánica Automotriz</option>
+                    <option value="Ing. Civil">Ing. Civil</option>
+                    <option value="Lic. Ciencias Empresariales">Lic. Ciencias Empresariales</option>
+                    <option value="Lic. Matemáticas Aplicadas">Lic. Matemáticas Aplicadas</option>
+                </select>
                 <input type="number" placeholder="Teléfono" />
                 <input type="email" placeholder="Correo" />
-                <label htmlFor="file-upload" style={{color:'black'}}>Credencial UTM</label>
+                <label htmlFor="file-upload" style={{color:'black'}}>Credencial UTM:</label>
                 <input type="file" id="file-upload" accept="image/*" onChange={updateFileName} />
                 {imagePreview && <img src={imagePreview} alt="Vista previa" style={{ maxWidth: '100%', maxHeight: '200px', marginTop: '10px' }} />}
                 <span>{fileName ? `Credencial UTM: ${fileName}` : "Seleccione un archivo"}</span>
